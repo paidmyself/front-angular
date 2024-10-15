fafdd95efa625a0fcb970762707da4f7946a0205
fbansept <bansept.franck@gmail.com>
2024-10-15 17:00:28 +0200

    bouton supprimer

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -16,10 +16,22 @@ export class AccueilComponent {
       nom: 'Super',
       images: [
         'https://www.vsveicolispeciali.com/wp-content/uploads/2024/01/trdelnik-kurtoskalacs-street-food.jpg',
+        'https://www.vsveicolispeciali.com/wp-content/uploads/2024/01/trdelnik-kurtoskalacs-street-food.jpg',
       ],
     },
-    { nom: 'Bien', images: [] },
-    { nom: 'Moyen', images: [] },
+    {
+      nom: 'Bien',
+      images: [
+        'https://www.vsveicolispeciali.com/wp-content/uploads/2024/01/trdelnik-kurtoskalacs-street-food.jpg',
+      ],
+    },
+    {
+      nom: 'Moyen',
+      images: [
+        'https://www.vsveicolispeciali.com/wp-content/uploads/2024/01/trdelnik-kurtoskalacs-street-food.jpg',
+        'https://www.vsveicolispeciali.com/wp-content/uploads/2024/01/trdelnik-kurtoskalacs-street-food.jpg',
+      ],
+    },
     { nom: 'Pas top', images: [] },
     { nom: 'Nul', images: [] },
   ];
@@ -29,5 +41,17 @@ export class AccueilComponent {
       this.categories[0].images.push(this.saisieImage);
       this.saisieImage = '';
     }
+  }
+
+  deplacerImage(indexCategorie: number, indexImage: number, enDessous = true) {
+    const imageClique = this.categories[indexCategorie].images[indexImage];
+    this.categories[indexCategorie + (enDessous ? 1 : -1)].images.push(
+      imageClique,
+    );
+    this.categories[indexCategorie].images.splice(indexImage, 1);
+  }
+
+  supprimerImage(indexCategorie: number, indexImage: number) {
+    this.categories[indexCategorie].images.splice(indexImage, 1);
   }
 }
