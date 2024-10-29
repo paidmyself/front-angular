207f3c95bdc72cdc9e12c079e4c886d08e067343
fbansept <bansept.franck@gmail.com>
2024-10-29 15:27:57 +0100

    add connexion

--- src/app/connexion/connexion.component.ts
+++ src/app/connexion/connexion.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './connexion.component.html',
-  styleUrl: './connexion.component.scss'
+  styleUrl: './connexion.component.scss',
 })
 export class ConnexionComponent {
+  constructeurFormulaire = inject(FormBuilder);
 
+  formulaire: FormGroup = this.constructeurFormulaire.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required]],
+  });
 }
